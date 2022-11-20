f1f663e17b1c53493b88d8ea4b4e9885321831d7
Halauichi <99341095+Halauichi@users.noreply.github.com>
2022-11-19 21:59:39 -0600

    Add files via upload

---
+++ TargetHeartRateCalculator/TargetHeartRateCalculator/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace TargetHeartRateCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnDisplayTHR = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter your age";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(84, 224);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(700, 163);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Age";
+            this.columnHeader1.Width = 50;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Max Heart Rate";
+            this.columnHeader2.Width = 100;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Activity Type";
+            this.columnHeader3.Width = 125;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Target Heart Rate Zone";
+            this.columnHeader4.Width = 150;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(173, 44);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(228, 26);
+            this.txtAge.TabIndex = 2;
+            this.txtAge.TextChanged += new System.EventHandler(this.txtAge_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(173, 89);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(228, 28);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Activity Type";
+            // 
+            // btnDisplayTHR
+            // 
+            this.btnDisplayTHR.Location = new System.Drawing.Point(238, 177);
+            this.btnDisplayTHR.Name = "btnDisplayTHR";
+            this.btnDisplayTHR.Size = new System.Drawing.Size(115, 41);
+            this.btnDisplayTHR.TabIndex = 5;
+            this.btnDisplayTHR.Text = "Display THR";
+            this.btnDisplayTHR.UseVisualStyleBackColor = true;
+            this.btnDisplayTHR.Click += new System.EventHandler(this.btnDisplayTHR_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(368, 177);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(115, 41);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(506, 177);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(115, 41);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 450);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDisplayTHR);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "v";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnDisplayTHR;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+    }
+}
+
---
+++ TargetHeartRateCalculator/TargetHeartRateCalculator/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Your name: Husam Alauichi
+//Description: Heart Rate calculation
+//Date Created: 9/7/2020
+
+
+namespace TargetHeartRateCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+
+        // close window
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        // clear window
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            txtAge.Clear();
+            comboBox1.SelectedIndex = 0;
+            txtAge.Focus();
+        }
+
+
+        // load form 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.ActiveControl = txtAge;
+            comboBox1.Items.Add("-Select an Activity-");
+            comboBox1.Items.Add("Moderate");
+            comboBox1.Items.Add("Vigorous");
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void btnDisplayTHR_Click(object sender, EventArgs e)
+
+        {
+            listView1.Items.Clear();
+
+            if (txtAge.Text == String.Empty)
+            {
+                MessageBox.Show("Age field can not be empty");
+                txtAge.Focus();
+            }
+            else if (Convert.ToInt32(txtAge.Text) > 100 || (Convert.ToInt32(txtAge.Text) < 1) || (Convert.ToInt32(txtAge.Text) == 0))
+            {
+                MessageBox.Show("Please enter a number between 1 & 100");
+                txtAge.Focus();
+            }
+           
+            else if (comboBox1.SelectedIndex == 0)
+            {
+                MessageBox.Show("Please select an activity");
+            }
+            else
+            {
+                double HeartRate = 220 - Convert.ToInt32(txtAge.Text);
+                decimal lowEnd = (int)(HeartRate * .50);
+                decimal hiEnd = (int)(HeartRate * .70);
+                decimal VhiEnd = (int)(HeartRate * .85);
+                //System.Math.Round(VhiEnd ,1 );
+                if (comboBox1.SelectedIndex == 1)
+                {
+                    listView1.Items.Add(txtAge.Text);
+                    listView1.Items[0].SubItems.Add($"{HeartRate} (bpm)");
+                    listView1.Items[0].SubItems.Add($"{comboBox1.SelectedItem}");
+                    listView1.Items[0].SubItems.Add($"{System.Math.Round(lowEnd, 1)}  to {System.Math.Round(hiEnd,1)} (beats per minute)");
+
+                }
+                else if (comboBox1.SelectedIndex == 2)
+                {
+                    
+                    listView1.Items.Add(txtAge.Text);
+                    listView1.Items[0].SubItems.Add($"{HeartRate} (bpm)");
+                    listView1.Items[0].SubItems.Add($"{comboBox1.SelectedItem}");
+                    listView1.Items[0].SubItems.Add($"{System.Math.Round(hiEnd,1)}  to {System.Math.Round(VhiEnd,1)} (beats per minute)");
+                    
+                }
+            }
+        }
+
+     
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtAge_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
